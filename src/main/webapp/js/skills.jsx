392240bd5da521abec7e44a85ede7573afc51849
IamTechknow <techhexium@gmail.com>
2018-09-11 14:35:41 -0700

    Start on skills UI

--- src/main/webapp/js/skills.jsx
+++ src/main/webapp/js/skills.jsx
@@ -3,12 +3,37 @@ import React, {Component} from 'react';
 export default class Skills extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      classNum: props.data.classNum,
+      skillPoints: props.data.skillPoints,
+    };
+  }
+
+  getClass(classNum) {
+    switch(classNum) {
+        case 0:
+          return "Amazon";
+        case 1:
+          return "Sorceress";
+        case 2:
+          return "Necromancer";
+        case 3:
+          return "Paladin";
+        case 4:
+          return "Barbarian";
+        case 5:
+          return "Druid";
+        default:
+          return "Assassin";
+    }
   }
 
   render() {
     return (
         <div>
-            <p>Under construction</p>
+            <p>Class: {this.getClass(this.state.classNum)}</p>
+            <p>Skill points: {this.state.skillPoints}</p>
         </div>
     );
   }
