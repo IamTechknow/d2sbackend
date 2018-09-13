6cfe4a5c4641d7d94797b28d550dcdc237a2a751
Edmond Lee <techhexium@gmail.com>
2018-09-13 15:24:52 -0700

    Skill dependencies shown on tooltips

--- src/main/webapp/js/skill.jsx
+++ src/main/webapp/js/skill.jsx
@@ -9,7 +9,7 @@ export default class Skill extends Component {
 
   render() {
     return (
-      <label>{this.props.skillName + ` (Level ${this.props.skillLevel}+)`}
+      <label data-toggle="tooltip" data-placement="top" title={this.props.skillDeps}>{this.props.skillName + ` (Level ${this.props.skillLevel}+)`}
         <input type="number" className="skill form-control" name={this.props.formName} min="0" max="20" value={this.props.value} onChange={this.props.handler} />
       </label>
     );
--- src/main/webapp/js/skills.jsx
+++ src/main/webapp/js/skills.jsx
@@ -16,6 +16,11 @@ export default class Skills extends Component {
     };
   }
 
+  // Activate Bootstrap tooltips via jQuery
+  componentDidMount() {
+    $('[data-toggle="tooltip"]').tooltip();
+  }
+
   getClass(classNum) {
     switch(classNum) {
         case ZON:
@@ -35,6 +40,19 @@ export default class Skills extends Component {
     }
   }
 
+  getSkillDeps(depsArray) {
+    if(depsArray.length === 0) {
+        return "";
+    }
+
+    var skills = [];
+    for(var dep of depsArray) {
+        let offset = dep - ClassData[this.state.classNum].skills[0].id;
+        skills.push(ClassData[this.state.classNum].skills[offset].name);
+    }
+    return "Depends on " + skills.join(", ");
+  }
+
   render() {
     var allocated = this.props.data.allocated;
     return (
@@ -45,17 +63,17 @@ export default class Skills extends Component {
             <div className="form-row">
                 <div className="col-md-4">
                     { ClassData[this.state.classNum].skills.slice(0, 10).map((skill, i) =>
-                        <Skill key={skill.id} formName={`skill-${i}`} skillName={skill.name}  skillId={skill.id} skillLevel={skill.level} skillDeps={skill.deps} value={allocated[i]} handler={this.props.handler} />
+                        <Skill key={skill.id} formName={`skill-${i}`} skillName={skill.name}  skillId={skill.id} skillLevel={skill.level} skillDeps={this.getSkillDeps(skill.deps)} value={allocated[i]} handler={this.props.handler} />
                     )}
                 </div>
                 <div className="col-md-4">
                     { ClassData[this.state.classNum].skills.slice(10, 20).map((skill, i) =>
-                        <Skill key={skill.id} formName={`skill-${10 + i}`} skillName={skill.name}  skillId={skill.id} skillLevel={skill.level} skillDeps={skill.deps} value={allocated[10 + i]} handler={this.props.handler} />
+                        <Skill key={skill.id} formName={`skill-${10 + i}`} skillName={skill.name}  skillId={skill.id} skillLevel={skill.level} skillDeps={this.getSkillDeps(skill.deps)} value={allocated[10 + i]} handler={this.props.handler} />
                     )}
                 </div>
                 <div className="col-md-4">
                     { ClassData[this.state.classNum].skills.slice(20, 30).map((skill, i) =>
-                        <Skill key={skill.id} formName={`skill-${20 + i}`} skillName={skill.name}  skillId={skill.id} skillLevel={skill.level} skillDeps={skill.deps} value={allocated[20 + i]} handler={this.props.handler} />
+                        <Skill key={skill.id} formName={`skill-${20 + i}`} skillName={skill.name}  skillId={skill.id} skillLevel={skill.level} skillDeps={this.getSkillDeps(skill.deps)} value={allocated[20 + i]} handler={this.props.handler} />
                     )}
                 </div>
             </div>
