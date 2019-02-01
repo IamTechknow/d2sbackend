f74e07a21ccea64581c327501629b25564da2bfc
Edmond Lee <techhexium@gmail.com>
2019-02-01 00:01:52 -0800

    Populate the dashboard select elements

--- src/main/webapp/js/dashboard.jsx
+++ src/main/webapp/js/dashboard.jsx
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Switch from '@material-ui/core/Switch';
 
+import ItemData from './item-data';
+
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
 
     this.handleChangeFor = this.handleChangeFor.bind(this);
+    this.onSubTypeChange = this.onSubTypeChange.bind(this);
 
     this.state = {
       randomAttr: false,
       ethernal: false,
+      currSubType: ItemData["primary"][0],
     };
   }
 
@@ -21,7 +25,20 @@ export default class Dashboard extends Component {
     };
   }
 
+  onSubTypeChange(event) {
+    this.setState({
+      currSubType : event.target.value
+    });
+  }
+
+  renderOptionsFor(type) {
+    return ItemData[type].map(opt => (
+      <option key={opt}>{opt}</option>
+    ));
+  }
+
   render() {
+    const { currSubType } = this.state;
     return (
       <Paper style={{ padding: '16px' }}>
         <div className="d2Grid dashboardGrid">
@@ -34,15 +51,32 @@ export default class Dashboard extends Component {
             <ul className="list-group">
               <li className="list-group-item d2Grid dashboardRow">
                 <span className="dashboardItem">Type</span>
-                <select className="form-control dashboardOpt" />
+                <select id="primaryType" className="form-control" onChange={this.onSubTypeChange}>
+                  {
+                    this.renderOptionsFor("primary")
+                  }
+                </select>
+                <select id="subType" className="form-control">
+                  {
+                    this.renderOptionsFor(currSubType)
+                  }
+                </select>
               </li>
               <li className="list-group-item d2Grid dashboardRow">
-                <span className="dashboardItem">Version</span>
-                <select className="form-control dashboardOpt" />
+                <span className="dashboardItem">Quality</span>
+                <select className="form-control dashboardOpt">
+                  {
+                    this.renderOptionsFor("quality")
+                  }
+                </select>
               </li>
               <li className="list-group-item d2Grid dashboardRow">
                 <span className="dashboardItem">Rarity</span>
-                <select className="form-control dashboardOpt" />
+                <select className="form-control dashboardOpt">
+                  {
+                    this.renderOptionsFor("rarity")
+                  }
+                </select>
               </li>
               <li className="list-group-item d2Grid dashboardRow">
                 <span className="dashboardItem">Random attribute values</span>
---
+++ src/main/webapp/js/item-data.jsx
@@ -0,0 +1,77 @@
+// Item types, names, and ids
+module.exports = {
+  primary: [
+    'Armor',
+    'Shields',
+    'Melee Weapons',
+    'Ranged Weapons',
+    'Staves',
+    'Jewelry',
+    'Class Items',
+    'Miscellaneous',
+  ],
+  quality: [
+    'Normal',
+    'Exceptional',
+    'Elite',
+  ],
+  rarity: [
+    'Normal',
+    'Set',
+    'Unique',
+    'Runeword',
+    'Inferior',
+  ],
+  Armor: [
+    'Belts',
+    'Body Armor',
+    'Boots',
+    'Gloves',
+    'Helms',
+    'Circlets',
+  ],
+  Shields: [
+    'Shields',
+    'Paladin Shields',
+    'Necromancer Totems',
+  ],
+  'Melee Weapons': [
+    'Axes',
+    'Daggers',
+    'Maces',
+    'Polearms',
+    'Scepters',
+    'Spears',
+    'Swords',
+  ],
+  'Ranged Weapons': [
+    'Bows',
+    'Crossbows',
+    'Javelins',
+    'Throwing Weapons',
+  ],
+  Staves: [
+    'Staves',
+    'Wands',
+    'Sorceress Orbs',
+  ],
+  Jewelry: [
+    'No subtypes',
+  ],
+  'Class Items': [
+    'Amazon Weapons',
+    'Assassin Claws',
+    'Barbarian Helms',
+    'Druid Helms',
+    'Necromancer Totems',
+    'Paladin Shields',
+    'Sorceress Orbs',
+  ],
+  Miscellaneous: [
+    'Runes',
+    'Gems',
+    'Potions',
+    'Tomes',
+    'Other',
+  ],
+};
